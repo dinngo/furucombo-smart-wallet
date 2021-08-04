d6b4d3dcd16aea474f970e11562edaeb08c70ede
Robin Pan <robin@dinngo.co>
2021-08-04 19:33:05 +0800

    Add AAuth test

--- contracts/interfaces/IDSAuth.sol
+++ contracts/interfaces/IDSAuth.sol
@@ -5,4 +5,8 @@ interface IDSAuth {
     function authority() external view returns (address);
     function setOwner(address owner_) external;
     function setAuthority(address authority_) external;
+
+    function canCall(
+        address src, address dst, bytes4 sig
+    ) external view returns (bool);
 }
--- test/AAuth.test.js
+++ test/AAuth.test.js
@@ -16,12 +16,14 @@ const utils = web3.utils;
 const { expect } = require('chai');
 
 const { DS_PROXY_REGISTRY } = require('./utils/constants');
-const { evmRevert, evmSnapshot } = require('./utils/utils');
+const { evmRevert, evmSnapshot, getCallData } = require('./utils/utils');
 
 const IDSProxyRegistry = artifacts.require('IDSProxyRegistry');
 const IDSProxy = artifacts.require('IDSProxy');
 const IDSAuth = artifacts.require('IDSAuth');
 const AAuth = artifacts.require('AAuth');
+
+const FUNCTION_SIG_EXECUTE = '0x1cff79cd';
 
 contract('AAuth', function([_, user, someone1, someone2]) {
   let id;
@@ -45,22 +47,46 @@ contract('AAuth', function([_, user, someone1, someone2]) {
 
   describe('Create DSGuard And Set', function() {
     beforeEach(async function() {
-      const auth = await this.userProxy.authority.call();
-      console.log(`auth = ${auth}`);
       expect(await this.userProxy.authority.call()).to.be.zero;
     });
+
     it('normal', async function() {
-      const data = abi.simpleEncode('createAndSetAuth()');
+      const data = getCallData(AAuth, 'createAndSetAuth', []);
       const receipt = await this.userProxy.execute(this.aAuth.address, data, {
         from: user,
       });
 
       const newAuthAddress = await this.userProxy.authority.call();
       const newAuth = await IDSAuth.at(newAuthAddress);
-      console.log(`newAuthAddress = ${newAuthAddress}`);
-
       expect(newAuthAddress).to.be.not.zero;
       expect(await newAuth.owner.call()).to.be.eq(this.userProxy.address);
+    });
+
+    it('pre-permit', async function() {
+      const callers = [someone1];
+      const data = getCallData(AAuth, 'createAndSetAuthPrePermit', [callers]);
+      const receipt = await this.userProxy.execute(this.aAuth.address, data, {
+        from: user,
+      });
+
+      const newAuthAddress = await this.userProxy.authority.call();
+      const newAuth = await IDSAuth.at(newAuthAddress);
+      expect(newAuthAddress).to.be.not.zero;
+      expect(await newAuth.owner.call()).to.be.eq(this.userProxy.address);
+      expect(
+        await newAuth.canCall.call(
+          someone1,
+          this.userProxy.address,
+          FUNCTION_SIG_EXECUTE
+        )
+      ).to.be.true;
+      expect(
+        await newAuth.canCall.call(
+          someone2,
+          this.userProxy.address,
+          FUNCTION_SIG_EXECUTE
+        )
+      ).to.be.false;
     });
   });
 });
