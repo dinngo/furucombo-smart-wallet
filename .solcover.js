45360c923b644efb05f296eac2b4acf5aadad881
Mussina Lin <mussina@dinngo.co>
2022-07-21 17:31:04 +0800

    feat: delete test.sh

--- .solcover.js
+++ .solcover.js
@@ -1,11 +1,9 @@
 module.exports = {
-  client: require('ganache-cli'),
-  providerOptions: {
-    fork:
-      'https://polygon-mainnet.g.alchemy.com/v2/3xc5Eh1ui8o_MrySO2Bcn2GRQk04Ky_q',
-    port: 8555,
-    network_id: 1337,
-  },
-
-  skipFiles: ['mocks'],
+  skipFiles: [
+    './mocks',
+    './examples',
+    './interfaces',
+    './externals/trevi/interfaces',
+    './externals/trevi/libraries/boringcrypto/interfaces',
+  ],
 };
--- hardhat.config.js
+++ hardhat.config.js
@@ -25,33 +25,21 @@ module.exports = {
   },
   networks: {
     hardhat: {
-      // forking: {
-      //   url:
-      //     'https://polygon-mainnet.g.alchemy.com/v2/3xc5Eh1ui8o_MrySO2Bcn2GRQk04Ky_q',
-      // },
-      // chainId: 137,
-      initialBaseFeePerGas: 0,
+      forking: {
+        url:
+          'https://polygon-mainnet.g.alchemy.com/v2/3xc5Eh1ui8o_MrySO2Bcn2GRQk04Ky_q',
+      },
       accounts: {
         mnemonic:
           'dice shove sheriff police boss indoor hospital vivid tenant method game matter',
         path: "m/44'/60'/0'/0",
         initialIndex: 0,
       },
-    },
-    localhost: {
+      chainId: 137,
+      initialBaseFeePerGas: 0,
       gasPrice: 0,
       gas: 30000000,
       timeout: 900000,
-    },
-    coverage: {
-      url: 'http://127.0.0.1:8555/',
-      chainId: 1337,
-      forking: {
-        url:
-          'https://polygon-mainnet.g.alchemy.com/v2/3xc5Eh1ui8o_MrySO2Bcn2GRQk04Ky_q',
-        // blockNumber: 30397489,
-        // enabled: true,
-      },
     },
   },
   mocha: {
