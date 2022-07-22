003ce1196d12470644c1d0df655f195737596a83
Mussina Lin <mussina@dinngo.co>
2022-07-22 17:20:52 +0800

    feat: add [ @skip-on-coverage ] tag.

--- .solcover.js
+++ .solcover.js
@@ -3,7 +3,14 @@ module.exports = {
     './mocks',
     './examples',
     './interfaces',
+    './externals/furucombo/interface',
     './externals/trevi/interfaces',
     './externals/trevi/libraries/boringcrypto/interfaces',
+    './externals/furucombo/interface',
   ],
+
+  mocha: {
+    grep: '@skip-on-coverage', // Find everything with this tag
+    invert: true, // Run the grep's inverse set.
+  },
 };
